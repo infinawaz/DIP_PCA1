7d9305b04d9e2c021497c5da56d4c820b89cd534
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-06-24 17:19:49 +0530

    Questions 1 & 2

---
+++ pca1.py
@@ -0,0 +1,17 @@
+import os
+from PIL import Image
+
+def open_show_save_image(image_path, save_path):
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    
+    with Image.open(image_path) as img:
+        img.show()
+        img.save(save_path)
+    print(f"Image opened from {image_path} and saved to {save_path}")
+
+
+if __name__ == "__main__":
+    input_image_path = "E:/DIP PCA1/inputImage/inputOne.jpg"
+    save_image_path = "E:/DIP PCA1/savedImage/savedOne.jpg"
+
+    open_show_save_image(input_image_path, save_image_path)
